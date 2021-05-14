ac7d94c8ecaaf6a5c92bfc7d83cc4d55f3d03ceb
Mahesh Kancharla <maheshkancharla614@gmail.com>
2021-05-15 03:30:26 +0530

    Unlinked RN unimodules and fixes npm's audit issues

--- android/app/src/main/java/com/cliniqui/MainApplication.java
+++ android/app/src/main/java/com/cliniqui/MainApplication.java
@@ -1,9 +1,6 @@
 package com.cliniqui;
 
 
-// com.myapp should be your package name
-import com.cliniqui.generated.BasePackageList;
-
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -17,13 +14,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Arrays;
 
-import org.unimodules.adapters.react.ModuleRegistryAdapter;
-import org.unimodules.adapters.react.ReactModuleRegistryProvider;
-import org.unimodules.core.interfaces.SingletonModule;
-
 public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -38,11 +29,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          // Add unimodules
-          List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
-            new ModuleRegistryAdapter(mModuleRegistryProvider)
-          );
-          packages.addAll(unimodules);
           return packages;
         }
 
