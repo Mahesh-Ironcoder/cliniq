0a13b7227d193f8089b8a7a051211e240929ed97
Mahesh <51284518+Mahesh-Ironcoder@users.noreply.github.com>
2021-06-25 03:20:41 +0530

    Connects websocket and peer at home component level

--- components/Home.js
+++ components/Home.js
@@ -1,28 +1,213 @@
 import VideoStream from './VideoStream';
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 
 import {useIsFocused} from '@react-navigation/core';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import MoreOptions from './MoreOptions';
-// import {registerGlobals} from 'react-native-webrtc';
+import {
+  RTCPeerConnection,
+  RTCSessionDescription,
+  RTCIceCandidate,
+} from 'react-native-webrtc';
 
-//--------------------------Done with imports---------------------------------------------
+//--------------------------Done with imports----------------------------------------
 const Tab = createBottomTabNavigator();
 
+const URL = 'http://127.0.0.1:6061/ws';
+const configuration = {
+  iceServers: [
+    {url: 'stun:stun.l.google.com:19302'},
+    /* {
+      url: 'turn:numb.viagenie.ca',
+      credential: 'muazkh',
+      username: 'webrtc@live.com',
+    }, */
+  ],
+  sdpSemantics: 'unified-plan',
+};
+
 const Home = (props) => {
-  // useEffect(() => {
-  //   registerGlobals();
-  // }, []);
+  const [socket, setSocket] = useState(null);
+  const [peer, setPeer] = useState(null);
+  const [vitalsData, setVitalsData] = useState(null);
+  /**
+   * RTCPeer effect
+   */
+  useEffect(() => {
+    if (!peer) {
+      console.log('No peer connection');
+      return;
+    }
+    peer.onicecandidate = (event) => {
+      if (event.candidate) {
+        let data = {Msgtype: 'ice', IceCandidate: event.candidate};
+        console.log('Sending candidates...');
+        sendJsonMsg(data);
+      }
+    };
+    peer.onnegotiationneeded = async (event) => {
+      console.log('Negotiating with peer server...', event);
+      let offer = await peer
+        .createOffer()
+        .catch((e) => console.log('Error in creating an offer'));
+
+      await peer
+        .setLocalDescription(offer)
+        .catch('Error in setting the local description');
+
+      let data = {Msgtype: 'offer', sdp: offer.sdp};
+      console.log('Sending offer');
+      sendJsonMsg(data);
+    };
+  }, [peer]);
+
+  /**
+   * Web socket effect
+   */
+  useEffect(() => {
+    if (!socket) {
+      console.log('No socket to connect with');
+      return;
+    }
+
+    socket.onopen = (event) => {
+      console.log('Connection to the server is opened...!', event);
+      setPeer(new RTCPeerConnection(configuration));
+    };
+
+    socket.onmessage = (event) => {
+      console.log('Recived a message from the other side');
+      let data = JSON.parse(event.data);
+      switch (data.type) {
+        case 'answer':
+          console.log('Recieved answer from the peer: ', data);
+          handleAnswer(data, peer);
+          break;
+        case 'ice':
+          console.log(
+            'Recived ice candidate from remote peer',
+            data.IceCandidate,
+          );
+          // handleIceCandidate(data.IceCandidate);
+          break;
+        case 'result':
+          console.log('Recieved result from the peer: ', data.result);
+          handleResult(JSON.parse(data.result));
+          break;
+
+        default:
+          break;
+      }
+    };
+
+    socket.onclose = (event) => {
+      console.log('Closing the connection...');
+      handleStop();
+    };
+
+    socket.onerror = (event) => {
+      console.log('Error in connecting to websocket: ', event);
+    };
+  }, [socket, peer]);
+
+  useEffect(() => {
+    setSocket(new WebSocket(URL));
+    return () => {
+      console.log('Distructing', socket);
+      socket.close();
+      handleStop();
+    };
+  }, []);
+
+  /**
+   * utilities and handlers
+   */
+
+  //stream handlers
+  function handleStart(stream) {
+    peer.addStream(stream);
+  }
+
+  function handleStop() {
+    if (!peer) {
+      return;
+    }
+    if (peer.getTransceivers) {
+      peer.getTransceivers().forEach(function (transceiver) {
+        if (transceiver.stop) {
+          transceiver.stop();
+        }
+      });
+    }
+
+    //remove stream
+    let localStreams = peer.getLocalStreams();
+    localStreams.forEach((stream) => {
+      peer.removeStream(stream);
+    });
+
+    // close peer connection
+    peer.close();
+  }
+
+  function handleReset(stream) {
+    peer.removeStream(stream);
+  }
+
+  //socket handlers
+  function sendJsonMsg(data) {
+    socket.send(JSON.stringify(data));
+  }
+
+  //message event handlers
+  async function handleAnswer(answer, peer) {
+    console.log('answer received: ', typeof answer);
+    if (peer === null) {
+      console.log('No peer at answer');
+      return;
+    }
+    await peer
+      .setRemoteDescription(new RTCSessionDescription(answer))
+      .catch((e) => {
+        console.log('Error in setting the answer', e);
+      });
+  }
+
+  function handleResult(result) {
+    if (!result[8].value) {
+      return;
+    }
+    let data = {};
+    data['Blood Pressure'] = [
+      {title: 'Systolic', value: result[3].value},
+      {title: 'Diastolic', value: result[4].value},
+    ];
+    data['Saturation'] = result[0].value + '%';
+    data['Heart Rate'] = result[1].value + result[1].prefix;
+    data['Respiration'] = result[2].value + result[2].prefix;
+    data['Temperature'] = result[5].value + result[5].prefix;
+    // data['Saturation'] = respText[0].value + respText[0].prefix;
+    // console.log('Vitals data updating are: ', data);
+    setVitalsData(data);
+  }
+
+  async function handleIceCandidate(candidate) {
+    await peer.addIceCandidate(new RTCIceCandidate(candidate));
+  }
 
   const isFocused = useIsFocused();
 
   function HomeScreen() {
     return (
       <View style={homescreenStyles.homeContainer}>
-        <VideoStream />
+        <VideoStream
+          onStart={handleStart}
+          onReset={handleReset}
+          vitalsToDisplay={vitalsData}
+        />
       </View>
     );
   }
--- components/VideoStream.js
+++ components/VideoStream.js
@@ -1,138 +1,24 @@
-// import 'react-native-gesture-handler';
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {
-  mediaDevices,
-  RTCIceCandidate,
-  RTCPeerConnection,
-  RTCSessionDescription,
-  RTCView,
-} from 'react-native-webrtc';
+import {mediaDevices, RTCView} from 'react-native-webrtc';
 import AppButton from './AppButton';
 import VitalsCardContainer from './VitalsCardContainer';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const URL = 'http://127.0.0.1:6061/ws';
-const configuration = {
-  iceServers: [
-    {url: 'stun:stun.l.google.com:19302'},
-    {
-      url: 'turn:numb.viagenie.ca',
-      credential: 'muazkh',
-      username: 'webrtc@live.com',
-    },
-  ],
-  sdpSemantics: 'unified-plan',
-};
-// RTCC
-// const peer = new RTCPeerConnection(configuration);
-// const socket = new WebSocket(URL);
-
 const VideoStream = (props) => {
   const [localStream, setLocalStream] = useState(null);
-  // const [remoteStream, setRemoteStream] = useState(null);
-  const [socketActive, setSocketActive] = useState(false);
-  const [socket, setSocket] = useState(null);
-  // const [socket, setSocket] = useState(new WebSocket(URL));
-  const [peer, setPeer] = useState(new RTCPeerConnection(configuration));
   const [isScanning, setIsScanning] = useState(false);
-
-  const [vitalsData, setVitalsData] = useState(null);
-  // const vitalsRef = useRef(null);
-
-  useEffect(() => {
-    if (!socketActive) {
-      alert('Sockets is not active');
-      return;
-    }
-    peer.onicecandidate = (evt) => {
-      if (evt.candidate) {
-        console.log('Event cand: ', evt.candidate);
-        var data = {
-          Msgtype: 'ice',
-          IceCandidate: evt.candidate,
-        };
-        console.log('Sending candidates...');
-        sendJsonMsg(data);
-      }
-    };
-
-    // peer.onaddstream = (event) => {
-    //   console.log('Got peeer connection', event.stream.getVideoTracks());
-    //   setRemoteStream(event.stream);
-    // };
-    // peer.onnegotiationneeded = () => {
-    //   console.log('Neogtiation needed: ');
-    //   createOffer();
-    // };
-  }, [socketActive]);
-
-  useEffect(() => {
-    if (!socket) {
-      alert('NO socket');
-      return;
-    }
-    // setSocket(new WebSocket(URL));
-    socket.onopen = (event) => {
-      console.log('Connection to websocket is open');
-      setSocketActive(true);
-    };
-    socket.onmessage = (event) => {
-      console.log('Message recieved from Websocket: ');
-      let data = JSON.parse(event.data);
-      switch (data.type) {
-        case 'answer':
-          console.log('Recieved answer from the peer: ', data);
-          handleAnswer(data);
-          break;
-        case 'result':
-          console.log('Recieved result from the peer: ', data.result);
-          handleResult(JSON.parse(data.result));
-          // setVitalsData(data.result);
-          break;
-        case 'iceCandidate':
-          console.log(
-            'Recived ice candidate from remote peer',
-            data.IceCandidate,
-          );
-          handleIceCandidate(data.IceCandidate);
-        default:
-          console.log('no');
-          break;
-      }
-    };
-    socket.onclose = (event) => {
-      console.log('Closing connection to websocket: ');
-      // peer.close();
-      setSocketActive(false);
-    };
-    socket.onerror = (event) => {
-      console.log('Error in connecting to websocket: ', event);
-      setSocketActive(false);
-    };
-
-    return () => {
-      console.log('Distructing..');
-      // peer.close();
-      stop();
-      socket.close();
-    };
-  }, [socket]);
-
-  useEffect(() => {
-    if (!localStream) {
-      return;
-    }
-    setSocket(new WebSocket(URL));
-  }, [localStream]);
+  const [front, setFront] = useState(true);
 
   useEffect(() => {
     getStream(setLocalStream);
+    // return () => {
+    //   setIsScanning(false);
+    //   setLocalStream(null);
+    // };
   }, []);
 
-  function getStream(setLocalStream) {
-    let isFront = true;
-
+  function getStream() {
     mediaDevices.enumerateDevices().then((sourceInfos) => {
       // console.log(sourceInfos);
       let videoSourceId;
@@ -140,7 +26,7 @@ const VideoStream = (props) => {
         const sourceInfo = sourceInfos[i];
         if (
           sourceInfo.kind == 'videoinput' &&
-          sourceInfo.facing == (isFront ? 'front' : 'environment')
+          sourceInfo.facing == (front ? 'front' : 'environment')
         ) {
           videoSourceId = sourceInfo.deviceId;
         }
@@ -152,113 +38,24 @@ const VideoStream = (props) => {
             width: 640,
             height: 480,
             frameRate: 30,
-            facingMode: isFront ? 'user' : 'environment',
+            facingMode: front ? 'user' : 'environment',
             deviceId: videoSourceId,
           },
         })
         .then((stream) => {
           setLocalStream(stream);
-          peer.addStream(stream);
         })
         .catch((error) => {
           console.log('Error in stream', error);
-          setStream(null);
+          setLocalStream(null);
         });
     });
   }
 
-  async function handleAnswer(answer) {
-    await peer.setRemoteDescription(new RTCSessionDescription(answer));
-  }
-
-  async function handleIceCandidate(candidate) {
-    await peer.addIceCandidate(new RTCIceCandidate(candidate));
-  }
-
-  function handleResult(result) {
-    let data = {};
-    data['Blood Pressure'] = [
-      {title: 'Systolic', value: result[3].value},
-      {title: 'Diastolic', value: result[4].value},
-    ];
-    data['Saturation'] = result[0].value + '%';
-    data['Heart Rate'] = result[1].value + result[1].prefix;
-    data['Respiration'] = result[2].value + result[2].prefix;
-    data['Temperature'] = result[5].value + result[5].prefix;
-    // data['Saturation'] = respText[0].value + respText[0].prefix;
-    // console.log('Vitals data updating are: ', data);
-    setVitalsData(data);
-  }
-
-  async function createOffer() {
-    if (!socketActive) {
-      alert('Socket is not active');
-      return;
-    }
-    // console.log('Peer: ', peer);
-    let offer = await peer
-      .createOffer()
-      .catch((e) => console.log('Error in createOFFer: ', e));
-    await peer
-      .setLocalDescription(offer)
-      .catch((e) => console.log('Error in setting LD: ', e));
-    data = {Msgtype: 'offer', sdp: offer.sdp};
-    // console.log('Sending offer: ', data);
-    sendJsonMsg(data);
-  }
-
-  function start() {
-    createOffer();
-    setIsScanning(true);
-  }
-
-  function stop() {
-    if (peer.getTransceivers) {
-      peer.getTransceivers().forEach(function (transceiver) {
-        if (transceiver.stop) {
-          transceiver.stop();
-        }
-      });
-    }
-    // peer.
-    peer.removeStream(localStream);
-
-    // close peer connection
-    setTimeout(function () {
-      peer.close();
-    }, 500);
-  }
-
-  /*   function createOffer() {
-    console.log('Peer: ', peer);
-    peer
-      .createOffer()
-      .then((desc) => {
-        console.log('createOffer', desc);
-        peer.setLocalDescription(
-          desc,
-          () => {
-            console.log('setLocalDescription', peer.localDescription);
-            // SocketUtils.emitExchangeServerSdp(socketId, peer.localDescription);
-            data = {Msgtype: 'offer', sdp: offer.sdp};
-            sendJsonMsg(data);
-          },
-          (error) => console.log(' LD error : ' + error),
-        );
-      })
-      .catch((error) => console.log('Offer error : ' + error));
-  } */
-
-  function sendJsonMsg(data) {
-    socket.send(JSON.stringify(data));
-  }
-
   function handleFlip(e) {
     console.log('Flipped');
-  }
-
-  function handleReset(e) {
-    peer.removeStream(localStream);
+    localStream.getTracks().forEach((track) => track._switchCamera());
+    setFront((prev) => !prev);
   }
 
   return (
@@ -271,7 +68,7 @@ const VideoStream = (props) => {
         <RTCView
           streamURL={localStream.toURL()}
           objectFit={'cover'}
-          mirror={true}
+          mirror={front}
           style={{
             width: '100%',
             height: '100%',
@@ -285,7 +82,12 @@ const VideoStream = (props) => {
         </View>
       )}
       {isScanning ? (
-        <VitalsCardContainer onReTest={handleReset} pictureData={vitalsData} />
+        <VitalsCardContainer
+          onReTest={(e) => {
+            props.onReset(localStream);
+          }}
+          pictureData={props.vitalsToDisplay}
+        />
       ) : (
         <View style={styles.btnGrp}>
           <AppButton
@@ -296,7 +98,10 @@ const VideoStream = (props) => {
             textStyle={{color: 'white', fontSize: 18}}
             title="Start Scan"
             rounded
-            onPress={start}
+            onPress={(e) => {
+              props.onStart(localStream);
+              setIsScanning(true);
+            }}
           />
           <Icon.Button
             name="camera-rear"
@@ -311,23 +116,6 @@ const VideoStream = (props) => {
           </Text>
         </View>
       )}
-      {/* {remoteStream ? (
-        <RTCView
-          streamURL={remoteStream.toURL()}
-          objectFit={'contain'}
-          mirror={true}
-          zOrder={2}
-          style={{
-            width: 320,
-            height: 480,
-            position: 'absolute',
-            top: 10,
-            borderWidth: 2,
-            // right: 0,
-            // zIndex: -1,
-          }}
-        />
-      ) : null} */}
     </View>
   );
 };
